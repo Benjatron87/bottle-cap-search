b03d067e21fdfa6208cd6be62d45e8d0ec5ccf0b
Benjatron <benjt122@gmail.com>
2019-07-03 11:50:14 -0400

    got the navbar up.

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -1,14 +1,23 @@
-import React, { Component } from 'react';
-import './Navbar.css';
+import React from "react";
+import {Link} from "react-router-dom";
+import "./Navbar.css";
 
-class Navbar extends Component {
-    render() {
-        return (
-            <div>
-                
+const Navbar = () => {
+  return (
+      <div className="container-fluid navbar sticky-top">
+            
+            <div className="nav-item">
+                    <Link to="/" className="nav-link home">Bottle Cap Search</Link>
             </div>
-        );
-    }
-}
+
+            <div className="d-flex justify-content-center">
+
+                <div className="nav-item">
+                        <Link to="/images" className="nav-link images">All Images</Link>
+                </div>
+            </div>
+      </div>
+  );
+};
 
 export default Navbar;
---
+++ client/src/pages/Images.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Images extends Component {
+    render() {
+        return (
+            <div>
+                images
+            </div>
+        );
+    }
+}
+
+export default Images;
